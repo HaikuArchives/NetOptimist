9c99421491a0651ed7031578d66fb0ba091e88d3
skian <>
2002-02-26 22:30:35 +0000

    Added a navigator object. Improved document.write

--- jsbridge/jsctx.cc
+++ jsbridge/jsctx.cc
@@ -13,37 +13,51 @@ int io_stderr (void *context, unsigned char *buffer, unsigned int amount) {
 static JSMethodResult
 nojs_document_write_helper(void *instance_context, int argc, JSType *argv,
 		char *error_return, bool newline) {
-	if (argc != 1)
+	if (argc < 1)
 	{
 		strcpy (error_return, "wrong amount of arguments");
 		return JS_ERROR;
 	}
-
 	DocFormater *doc = (DocFormater*)instance_context;
-	switch(argv[0].type)
-	{
-	case JS_TYPE_STRING:
-		doc->InsertText((char*)argv[0].u.s->data, argv[0].u.s->len);
-		break;
-	case JS_TYPE_DOUBLE:
-		{
-		char num[100];
-		int nb = sprintf(num, "%f", argv[0].u.d);
-		doc->InsertText(num, nb);
-		}
-		break;
-	case JS_TYPE_INTEGER:
-		{
-		char num[100];
-		int nb = sprintf(num, "%ld", argv[0].u.i);
-		doc->InsertText(num, nb);
-		}
-		break;
-	default:
-		strcpy (error_return, "illegal argument");
-		return JS_ERROR;
-	}
 
+	for (int i = 0; i<argc; i++) {
+		JSType *arg = &argv[i];
+		switch(arg->type)
+		{
+		case JS_TYPE_STRING:
+			doc->InsertText((char*)arg->u.s->data, arg->u.s->len);
+			break;
+		case JS_TYPE_DOUBLE:
+			{
+			char num[100];
+			int nb = sprintf(num, "%f", arg->u.d);
+			doc->InsertText(num, nb);
+			}
+			break;
+		case JS_TYPE_INTEGER:
+			{
+			char num[30];
+			int nb = sprintf(num, "%ld", arg->u.i);
+			doc->InsertText(num, nb);
+			}
+			break;
+		case JS_TYPE_NULL:
+			{
+			const char nullstring[] = "(null)";	// XXX not sure we should display this
+			doc->InsertText(nullstring, sizeof(nullstring)-1);
+			}
+			break;
+		case JS_TYPE_UNDEFINED:
+			{
+			const char nullstring[] = "(undefined)";	// XXX not sure what we should display here
+			doc->InsertText(nullstring, sizeof(nullstring)-1);
+			}
+			break;
+		default:
+			strcpy (error_return, "illegal argument type");
+			return JS_ERROR;
+		}
+	}
 	if (newline) doc->InsertText("\n", 2);
 
 	return JS_OK;
@@ -91,6 +105,18 @@ nojs_window_open(JSClassPtr , void *instance_context, JSInterpPtr ,
 	return JS_OK;
 }
 
+static JSMethodResult 
+nojs_navigator_javaEnabled(JSClassPtr , void *instance_context, JSInterpPtr ,
+	    int argc, JSType *argv, JSType *return_value, char *error_return) {
+	if (argc >= 1) {
+		strcpy (error_return, "wrong amount of arguments");
+		return JS_ERROR;
+	}
+	return_value->type = JS_TYPE_BOOLEAN;
+	return_value->u.i = false;	// No, Java is not supported :-(
+	return JS_OK;
+}
+
 static JSMethodResult
 nojs_window_url(JSClassPtr , void *instance_context, JSInterpPtr ,
 		int setp, JSType *value, char *error_return) {
@@ -109,6 +135,35 @@ nojs_window_url(JSClassPtr , void *instance_context, JSInterpPtr ,
 		return JS_ERROR;
 	} else {
 		fprintf(stderr, "nojs_window_url : trying to read property\n");
+		// XXX TODO
+		return JS_OK;
+	}
+}
+
+static JSMethodResult
+nojs_navigator_appCodeName(JSClassPtr , void *instance_context, JSInterpPtr interp,
+		int setp, JSType *value, char *error_return) {
+	if (setp) {
+		fprintf(stderr, "nojs_navigator_appCodeName : trying to set property\n");
+		// XXX Do we have a read-only property ?
+		return JS_ERROR;
+	} else {
+		const char appCodeName[] = "NetO";
+		js_type_make_string (interp, value, (unsigned char*)appCodeName, sizeof(appCodeName)-1);
+		return JS_OK;
+	}
+}
+
+static JSMethodResult
+nojs_navigator_appName(JSClassPtr , void *instance_context, JSInterpPtr interp,
+		int setp, JSType *value, char *error_return) {
+	if (setp) {
+		fprintf(stderr, "nojs_navigator_appName : trying to set property\n");
+		// XXX Do we have a read-only property ?
+		return JS_ERROR;
+	} else {
+		const char appName[] = "NetOptimist";
+		js_type_make_string (interp, value, (unsigned char*)appName, sizeof(appName)-1);
 		return JS_OK;
 	}
 }
@@ -160,12 +215,24 @@ void JsCtx::Init(DocFormater *document) {
 	cls = js_class_create (NULL, NULL, 0 /* no_auto_destroy */, NULL /*ctor*/);
 	js_class_define_method (cls, "open", JS_CF_STATIC, nojs_window_open);
 	js_class_define_property (cls, "url", 0, nojs_window_url);
-	js_define_class (m_jsdata->interp, cls, "NetOptimistDocument");
+	js_define_class (m_jsdata->interp, cls, "NetOptimistWindow");
 
 	// window object
 	JSType * windowObject = new JSType;
 	js_instantiate_class (m_jsdata->interp, cls, document, NULL, windowObject);
 	js_set_var (m_jsdata->interp, "window", windowObject);
+
+	// navigator class
+	cls = js_class_create (NULL, NULL, 0 /* no_auto_destroy */, NULL /*ctor*/);
+	js_class_define_property (cls, "appCodeName", JS_CF_IMMUTABLE, nojs_navigator_appCodeName);
+	js_class_define_property (cls, "appName", JS_CF_IMMUTABLE, nojs_navigator_appName);
+	js_class_define_method (cls, "javaEnabled", JS_CF_STATIC, nojs_navigator_javaEnabled);
+	js_define_class (m_jsdata->interp, cls, "NetOptimistNavigator");
+
+	// navigator object
+	JSType * navigatorObject = new JSType;
+	js_instantiate_class (m_jsdata->interp, cls, NULL, NULL, navigatorObject);
+	js_set_var (m_jsdata->interp, "navigator", navigatorObject);
 }
 
 void JsCtx::Execute(const char *t) {
