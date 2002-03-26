405779a6b49f04bb7a76ed1884e83bc83c9e6906
skian <>
2002-03-26 20:23:24 +0000

    This file defines various html alignment attributes

---
+++ Engine/Alignment.h
@@ -0,0 +1,14 @@
+#ifndef Alignment_H
+#define Alignment_H
+
+enum Alignment {
+	al_bottom,
+	al_center,
+	al_top,
+	al_left,
+	al_right
+	// TODO anything else ?
+	// From www.blooberry.com/indexdot/htm : Texttop | Middle | Absmiddle | Baseline | Bottom | Absbottom
+};
+
+#endif
